23f02cc4251b17f8de72b4bfef79057b3f75ff2a
Ibrahim Duman <dumanibrahm@gmail.com>
2023-08-03 15:03:53 +0300

    Sign in page design done.

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:pigeon_app/Widgets/social_button.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade200,
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                "Sign In",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              SocialLoginButton(
+                buttonText: "Sign In with Google",
+                butonColor: Colors.white,
+                textColor: Colors.black87,
+                buttonIcon: Image.asset(
+                  "images/Google-logo.png",
+                  width: 30,
+                  height: 30,
+                ),
+                onPressed: () {},
+              ),
+              SocialLoginButton(
+                buttonText: "Sign In with Facebook",
+                butonColor: Colors.blue,
+                textColor: Colors.white,
+                buttonIcon: Image.asset(
+                  "images/facebook-logo.png",
+                  width: 30,
+                  height: 30,
+                ),
+                onPressed: () {},
+              ),
+              SocialLoginButton(
+                buttonText: "Sign In with Email and Password",
+                butonColor: const Color(0xFF334092),
+                textColor: Colors.white,
+                buttonIcon: const Icon(
+                  Icons.mail,
+                  color: Colors.white,
+                  size: 32,
+                ),
+                onPressed: () {},
+              ),
+              SocialLoginButton(
+                buttonText: "Anonymous Sign In",
+                butonColor: Colors.teal,
+                textColor: Colors.white,
+                buttonIcon: const Icon(
+                  Icons.supervised_user_circle,
+                  color: Colors.white,
+                  size: 32,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
